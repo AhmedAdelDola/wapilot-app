f4b9d3f3f39c40f7d222e185cb87fc1b896ad0b9
AhmedAdelDola <139361482+AhmedAdelDola@users.noreply.github.com>
2026-08-19 12:08:19 +0300

    Add Firebase config files for EAS Build

--- app.config.ts
+++ app.config.ts
@@ -2,14 +2,14 @@ import { ConfigContext, ExpoConfig } from 'expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig => {
   return {
-    name: 'Wapilot',
-    slug: process.env.EXPO_PUBLIC_APP_SLUG || 'wapilot-mobile',
+    name: 'Message Pro',
+    slug: process.env.EXPO_PUBLIC_APP_SLUG || 'message-pro-mobile',
     version: '1.0.0',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
     newArchEnabled: false,
-    scheme: 'wapilotapp',
+    scheme: 'messagepro',
     splash: {
       image: './assets/splash.png',
       resizeMode: 'contain',
@@ -18,7 +18,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     },
     ios: {
       supportsTablet: true,
-      bundleIdentifier: 'com.wapilot.app',
+      bundleIdentifier: 'com.messagepro.app',
       infoPlist: {
         NSCameraUsageDescription:
           'This app requires access to the camera to upload images and videos.',
@@ -31,18 +31,33 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     },
     android: {
       adaptiveIcon: { foregroundImage: './assets/adaptive-icon.png', backgroundColor: '#ffffff' },
-      package: 'com.wapilot.app',
+      package: 'com.messagepro.app',
+      googleServicesFile: './google-services.json',
       permissions: ['android.permission.CAMERA', 'android.permission.RECORD_AUDIO'],
     },
     extra: {
       eas: {
-        projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+        projectId: '9819b83a-87f0-46ff-be76-1ff2bcd2c2b8',
       },
     },
-    owner: 'wapilot',
+    owner: 'ahmed-adel12',
     plugins: [
       'expo-font',
       'expo-splash-screen',
+      'expo-image-picker',
+      'expo-document-picker',
+      [
+        '@react-native-firebase/app',
+        {
+          googleServicesFile: './google-services.json',
+        },
+      ],
+      [
+        '@react-native-firebase/messaging',
+        {
+          googleServicesFile: './GoogleService-Info.plist',
+        },
+      ],
     ],
     androidNavigationBar: { backgroundColor: '#ffffff' },
   };
